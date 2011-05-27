f958454be5cc095dbb74d7eafb04c33638bbde1d
Mike Bevz <me@mikebevz.com>
2011-05-27 13:30:38 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weminds::Application.config.secret_token = 'd1f2023dbfa848c7a08e16a13a179a743dc6bb3e4330531131fb640a0ed4b816d1c80123ffbed042901e6d43ccf126c763a3a8d8c3fd96efa7be5835cf6b22b9'
